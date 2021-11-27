b420b9665effb113ba51a81840c3a60ba87a28b5
igdkaur <igdkaur@gmail.com>
2021-11-27 15:59:15 +0530

    added buttons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const Time = () => <span className="time">3h ago</span>;
+const ReplyButton = () => <i className="fa fa-reply reply-button" />;
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
+const LikeButton = () => <i className="fa fa-heart like-button" />;
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+);
+
 function Avatar() {
   return (
     <img
@@ -18,7 +26,14 @@ function Tweet() {
       <Avatar />
       <div className="content">
         <NameWithHandle />
+        <Time />
         <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
       </div>
     </div>
   );
